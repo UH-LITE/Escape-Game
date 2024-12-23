e3e96ebbf52ac212b1a5af44348147949bcb826f
Susie Gronseth <slgronse@cougarnet.uh.edu>
2024-12-23 07:28:38 -0600

    2nd publish

--- story_content/6JZcG1DQNOB_transcripts.js
+++ story_content/6JZcG1DQNOB_transcripts.js
@@ -1,5 +1,5 @@
 
 (function() {
-    const data = {"transcripts":[{"name":"captions","cues":[{"start":1533,"text":"I’m finally outside..."},{"start":3383,"text":"but I can’t help feeling like I should’ve brought Lumy with me."},{"start":7066,"text":"Anyway,"},{"start":7933,"text":"maybe I’ll buy one of those robotic vacuum cleaners everyone’s raving about."},{"start":12133,"text":"It’s time to go home."}]}]};
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":2150,"text":"I’m finally outside..."},{"start":4000,"text":"but I can’t help feeling like I should’ve brought Lumy with me."},{"start":7683,"text":"Anyway,"},{"start":8550,"text":"maybe I’ll buy one of those robotic vacuum cleaners everyone’s raving about."},{"start":12750,"text":"It’s time to go home."}]}]};
     window.globalLoadJsAsset('story_content/6JZcG1DQNOB_transcripts.js', JSON.stringify(data));
 })();
